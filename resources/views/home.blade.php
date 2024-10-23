b04eb2a7f1cedd2d2914f7d20e886ecf2c058152
pedrolaskawski <pedro_laskawski_dev@hotmail.com>
2024-10-22 23:19:23 -0300

    imagens ajustes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,6 +11,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
     </script>
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
 </head>
 
 <body>
@@ -38,19 +39,32 @@
         <div class="row">
             <div id="carouselExample" class="carousel carousel-dark slide mt-5">
                 <div class="carousel-inner">
-                    <div class="carousel-item active text-center">
-                        <img src='{{ asset('storage/pc-1.webp') }}' height="250" class="d-block w-50 mx-auto">
-                        <button type="button" class="btn btn-primary mt-2">Saiba mais</button>
+                    <div class="carousel-item active text-center">       {{--  w-100 w-sm-50                --}}
+                        <img src='{{ asset('storage/energia_eolica_renovavel.webp') }}' height="250" class="d-block w-50 mx-auto carrousel-img">
+                        <div class="carousel-caption d-block">
+                            <h3>Soluções para energia eólica</h3>
+                            <p>Soluções com o Elipse E3</p>
+                            <button type="button" class="btn btn-primary">Saiba mais</button>
+                        </div>
                     </div>
                     <div class="carousel-item text-center">
-                        <img src='{{ asset('storage/pc-2.jpg') }}' height="250" class="d-block w-50 mx-auto">
-                        <button type="button" class="btn btn-primary">Saiba mais</button>
+                        <img src='{{ asset('storage/usina_eletrica_por_do_sol.webp') }}' height="250" class="d-block w-50 mx-auto carrousel-img">
+                        <div class="carousel-caption d-block">
+                            <h5>Soluções para energia elétrica</h5>
+                            <p>Descubra mais com as ferramentas D25 e D400.</p>
+                            <button type="button" class="btn btn-primary">Saiba mais</button>
+                           
+                        </div>
                     </div>
                     <div class="carousel-item text-center">
-                        <img src='{{ asset('storage/pc-3.jpg') }}' height="250" class="d-block w-50 mx-auto">
-                        <button type="button" class="btn btn-primary">Saiba mais</button>
+                        <img src='{{ asset('storage/pc-3.jpg') }}' height="250" class="d-block  w-50 mx-auto carrousel-img">
+                        <div class="carousel-caption d-block">
+                            <h5>First slide label</h5>
+                            <p>Some representative placeholder content for the first slide.</p>
+                            <button type="button" class="btn btn-primary">Saiba mais</button>
+                        </div>
                     </div>
-                    
+
                 </div>
 
                 <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample"
@@ -65,23 +79,24 @@
                 </button>
             </div>
         </div>
-        <div class="row d-flex justify-content-center mt-5">
-            <div class="card col-2 m-1">
-                <div class="card-body">
-                  This is some text within a card body.
-                </div>
-              </div>
-              <div class="card col-2 m-1">
-                <div class="card-body">
-                  This is some text within a card body.
-                </div>
-              </div>
-              <div class="card col-5 m-1">
-                <div class="card-body">
-                  This is some text within a card body.
-                </div>
-              </div>
+    </div>
+    <div class="row d-flex justify-content-center mt-5">
+        <div class="card col-2 m-1">
+            <div class="card-body">
+                This is some text within a card body.
+            </div>
         </div>
+        <div class="card col-2 m-1">
+            <div class="card-body">
+                This is some text within a card body.
+            </div>
+        </div>
+        <div class="card col-5 m-1">
+            <div class="card-body">
+                This is some text within a card body.
+            </div>
+        </div>
+    </div>
     </div>
 </body>
 
